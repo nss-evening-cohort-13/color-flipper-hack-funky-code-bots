228f6377891dc6c0256da5e75af9717b7ceb1b78
Summer Duke <esrduke@gmail.com>
2020-07-28 19:35:38 -0500

    starter function for color flipper

---
+++ setup/app.js
@@ -0,0 +1,15 @@
+// console.log('yoooo');
+
+
+
+
+
+const colorFlipper = () => {
+    return array[i].hex[Math.floor(Math.random() * array.length)];
+}
+
+const init = () => {
+    console.log(colorFlipper());
+}
+
+init();
