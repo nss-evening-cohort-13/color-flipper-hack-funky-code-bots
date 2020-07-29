152d5f924370cbc68986fac5abee9693abfb9a96
danapham <danapham12@gmail.com>
2020-07-28 19:32:07 -0500

    started the function

---
+++ setup/app.js
@@ -0,0 +1,20 @@
+const handleButtonClick = (e) => {
+  const buttonId = e.target.id;
+
+  
+}
+
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const simpleBuilder = (simpArray) => {
+  let domString = '';
+
+  for (let i = 0; i < simpArray.length; i++) {
+    domString += ``
+  }
+}
+
+const 
